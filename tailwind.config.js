e20a59f91fc923b44e57b652dced0f81c1d427de
Aymane Naouadir <aymane.naouadir18@gmail.com>
2023-03-25 21:45:44 +0000

    add a config in tailwindcss about flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-   "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-   "./node_modules/flowbite-react/**/*.js",
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/flowbite-react/**/*.js",
+		"./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
