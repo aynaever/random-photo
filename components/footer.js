37d63850d4cc912ee4acfde6f90a24641ac64730
Aymane Naouadir <aymane.naouadir18@gmail.com>
2023-03-25 16:28:45 +0000

    changing a tag to Link component and updating href

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 <footer class="bg-white shadow dark:bg-gray-800">
     <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">RandomPG™</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" class="hover:underline">RandomPG™</Link>. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
