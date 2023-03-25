0da32f8c1db9a060993e5cf8b7f6a76ed5a5b887
Aymane Naouadir <aymane.naouadir18@gmail.com>
2023-03-25 16:48:11 +0000

    add sign in link to header.js

--- components/header.js
+++ components/header.js
@@ -24,6 +24,9 @@ export default function Header() {
         <li>
           <Link href="/contact" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
         </li>
+        <li>
+          <Link href="/sign-in" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign in</Link>
+        </li>
       </ul>
     </div>
   </div>
