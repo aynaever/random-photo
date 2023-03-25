9ffb420b4b762536d66004966473f899b7846aa9
Aymane Naouadir <aymane.naouadir18@gmail.com>
2023-03-25 21:21:41 +0000

    add Head component and change title and add meta tags

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,16 @@
 import Layout from '../components/layout';
+import Head from 'next/head';
 
 export default function Contact() {
 	return (
-		<Layout>
-		</Layout>
+		<>
+			<Head>
+				<title>Contact</title>
+				<meta name="description" content="Contact Page" />
+			</Head>
+			<Layout>
+				<h1 className="text-center text-3xl sm:text-4xl md:text-5xl font-bold my-8">Contact</h1>
+			</Layout>
+		</>
 	);
 }
